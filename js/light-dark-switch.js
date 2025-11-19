98c8d60db04fe3d92f98ff677b56eb3095a1beff
Biscuitxxx <1968950864@qq.com>
2025-11-19 11:20:06 +0800

    Site updated: 2025-11-19 11:20:05

--- js/light-dark-switch.js
+++ js/light-dark-switch.js
@@ -1,12 +1,18 @@
 let themeFunc = async function () {
     let btn = document.getElementById("theme-btn");
     let root = document.documentElement;
-    // 新增：默认设置为light模式
-    root.setAttribute('theme', 'light');
-    localStorage.setItem('theme', 'light');
+
+    // 初始化时读取本地存储的主题，没有的话默认亮色
+    if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && root.getAttribute('theme') !== 'light')) {
+        root.setAttribute('theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        root.setAttribute('theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
 
     btn.addEventListener('click', e => {
-        if (root.getAttribute('theme') == 'dark') {
+        if (root.getAttribute('theme') === 'dark') {
             root.setAttribute('theme', 'light');
             localStorage.setItem('theme', 'light');
         } else {
@@ -16,4 +22,3 @@ let themeFunc = async function () {
     });
 };
 themeFunc();
-
